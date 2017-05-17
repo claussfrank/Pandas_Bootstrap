080e8a25ccc9904e5f412b0082cf9f2d9b4b602d
Clauss Franco <Clauss Franco>
2017-05-16 20:18:30 -0500

    acomodo con bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+//Estamos  llamando los  elementos  que vamos  a  modificar
+var imagenes= document.getElementsByClassName("imagenClose");
+var botones= document.getElementsByClassName("boton");
+var origen = document.getElementById("origen");
+var extincion = document.getElementById("extincion");
+console.log(restaurar);
+
+/*funcionabilidad de eliminacion de imagenes*/
+//recorriendo la clase imagenClose
+for (var i=0; i<imagenes.length; i++){
+  imagenes[i].addEventListener("click",cerrarImagen);
+}
+
+function cerrarImagen(){
+  this.style.display="none";
+}
+
+
+botones[2].addEventListener("click",restaurar);
+function restaurar(){
+  for(var i=0; i<imagenes.length;i++){
+    imagenes[i].style.display="flex";
+  }
+}
+
+botones[0].addEventListener("click",mostraOrigen);
+
+function mostraOrigen(){
+  if (origen.style.visibility =="hidden"){
+    origen.style.visibility = "visible";
+  }else{
+    origen.style.visibility = "hidden";
+  }
+}
+
+
+botones[1].addEventListener("click",mostrarExtincion);
+
+function mostrarExtincion(){
+  if (extincion.style.visibility == "hidden"){
+    extincion.style.visibility = "visible";
+  }else {
+    extincion.style.visibility="hidden";
+  }
+}
